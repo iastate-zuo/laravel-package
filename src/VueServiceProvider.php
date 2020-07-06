dbe228c26cf0ab525e2d99bc31eb1fd881a10e37
ZUO, JACOB [ITUIS] <zuo@iastate.edu>
2020-07-06 16:50:15 -0500

    adding files

---
+++ src/VueServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Webdev\Vuetest;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class VueServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+		// Merge views together
+		$this->loadViewsFrom(__DIR__ . '/../resources/views', 'vuetest');
+	}
+
+	public function boot()
+	{
+		if ($this->app->runningInConsole())
+		{
+			$this->publishResources();
+		}
+
+		$this->publishComponents();
+	}
+
+	protected function publishResources()
+	{
+		$this->publishes([
+			__DIR__ . '/../resources/views' => resource_path('views/vendor/vuetest'),
+		], 'vuetest');
+	}
+
+	protected function publishComponents()
+	{
+
+	}
+}
